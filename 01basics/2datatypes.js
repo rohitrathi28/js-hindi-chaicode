4af6d6ca635feeaffdaf5c0de1a023d274e64f2f
rohitrathi28 <111492271+rohitrathi28@users.noreply.github.com>
2024-07-08 11:46:26 +0000

    variables and data types

---
+++ 01basics/2datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat js as new version
+
+// alert(3+3) we aer useing node js not browser
+console.log(3+3);
+console.log("rr");
+
+
+let name = "rohit"
+let age = 20
+let logged = true
+
+/**
+number = 2 ^ 53
+bigint = for large numbers
+string = "assembly of characters"
+boolean = true or false
+null = stand alone value (type = object)
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = uniquness
+ */
+
+
+console.log(typeof null); // objest
+console.log(typeof undefined); //underfined
